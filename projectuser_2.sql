5d3932cae100332598de75532107fb7abe2324f8
716 <525223688@qq.com>
2013-12-04 16:09:12 +0800

    数据库结构。
    Signed-off-by: 716 <525223688@qq.com>

---
+++ projectuser_2.sql
@@ -0,0 +1,38 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.2.2
+-- http://www.phpmyadmin.net
+--
+-- 主机: 127.0.0.1
+-- 生成日期: 2013 年 12 月 04 日 09:08
+-- 服务器版本: 5.5.27
+-- PHP 版本: 5.4.7
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- 数据库: `projectuser`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `tp_user`
+--
+
+CREATE TABLE IF NOT EXISTS `tp_user` (
+  `Pid` double NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) NOT NULL,
+  `pwd` varchar(10) NOT NULL,
+  PRIMARY KEY (`Pid`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=58 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
